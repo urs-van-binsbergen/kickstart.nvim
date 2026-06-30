af94b1aed02995c63774a18675036bba824c4dc1
uvb <urs.vanbinsbergen@zhdk.ch>
2026-06-30 15:00:58 +0200

    some essentials opts

--- init.lua
+++ init.lua
@@ -171,6 +171,14 @@ do
   -- instead raise a dialog asking if you wish to save the current file(s)
   -- See `:help 'confirm'`
   vim.o.confirm = true
+
+  -- uvb
+  vim.opt.expandtab = true
+  vim.opt.shiftwidth = 2
+  vim.opt.tabstop = 2
+  vim.opt.softtabstop = 2
+  vim.opt.wrap = true
+  vim.opt.linebreak = true
 end
 
 -- ============================================================
